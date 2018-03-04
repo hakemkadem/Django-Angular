2fbb8b078810fdabdcc79e2bb83f6c6771f184dd
hakemkadem <hakemk21@gmail.com>
2018-03-05 02:27:35 +0300

    First Angular new

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['secure-savannah-75111.herokuapp.com/','127.0.0.1']
 
 # Application definition
 
